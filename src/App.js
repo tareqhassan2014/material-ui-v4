c0d90ebf00671008e8932f1ec917619e3242b9c4
Tareq Hassan <tareqhassan2014@gmail.com>
2021-12-10 19:36:09 +0600

    add assets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { ThemeProvider } from "@material-ui/core";
 import { Route, Routes } from "react-router-dom";
+import theme from "./components/theme/theme";
 import Home from "./pages/Home";
 
 const App = () => {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <Routes>
                 <Route index element={<Home />} />
             </Routes>
-        </>
+        </ThemeProvider>
     );
 };
 
